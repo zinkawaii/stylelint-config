757284c63f20ef6afa65374b6a0e6fb2038d3666
KazariEX <1364035137@qq.com>
2024-03-22 00:00:28 +0800

    feat: 集成配置对象

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ import order from "./lib/order.js";
 import standard from "./lib/standard.js";
 import stylistic from "./lib/stylistic.js";
 
-export default {
-    order,
-    standard,
-    stylistic
+export default function() {
+    return {
+        extends: [
+            "stylelint-config-standard-scss",
+            "stylelint-config-html/vue"
+        ],
+        plugins: [
+            "stylelint-order",
+            "@stylistic/stylelint-plugin"
+        ],
+        rules: {
+            ...order,
+            ...standard,
+            ...stylistic
+        }
+    };
 };
